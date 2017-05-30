76e876ed21fbf80ef90a4afb3da10ed99201662e
Software Engineer <Software Engineer>
2017-05-30 17:32:10 +0200

    final3

--- server/api/product/product.model.js
+++ server/api/product/product.model.js
@@ -1,34 +1,36 @@
 'use strict';
 
-import mongoose from 'mongoose';
-var Schema = mongoose.Schema,
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _defineProperty2 = require('babel-runtime/helpers/defineProperty');
+
+var _defineProperty3 = _interopRequireDefault(_defineProperty2);
+
+var _ref;
+
+var _mongoose = require('mongoose');
+
+var _mongoose2 = _interopRequireDefault(_mongoose);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var Schema = _mongoose2.default.Schema,
     ObjectId = Schema.ObjectId;
-    
-var ProductSchema = new mongoose.Schema({
-  sku:            String,
-  name:           String,
-  nameLower:      String,
-  phone:          String,
-  email:          String,
-  slug:           String,
-  logo:           Array,
-  info:           String,
-  website:        String,
-  terms:          String,
-  category:       { type: ObjectId, ref: 'Category' },
-  status:         String,
-  brand:          { type: ObjectId, ref: 'Brand' },
-  description:    String,
-  variants:       [{ image : String, sku: String, name : String, price : Number, size : String,model: String, maxSize : String, formart : String ,preview :Object ,stockLevel:Number,UnitsInStock:Number,UnitsOnOrder:Number,ReorderLevel:Number,slots:Array}],
-  features:       Array,
-  keyFeatures:    Array,
-  stats:         [{key:String, val:String}],
-  active:         { type: Boolean, default: true },
-  uid:            String,
-  created_at    : { type: Date },
-  updated_at    : { type: Date, default: Date.now },
-  updated:        {type: Date, default: Date.now}
-}, { versionKey: false });
 
+var ProductSchema = new _mongoose2.default.Schema((_ref = {
+  sku: String,
+  name: String,
+  nameLower: String,
+  phone: String,
+  email: String,
+  slug: String,
+  logo: Array,
+  info: String,
+  website: String,
+  terms: String
+}, (0, _defineProperty3.default)(_ref, 'slug', String), (0, _defineProperty3.default)(_ref, 'category', { type: ObjectId, ref: 'Category' }), (0, _defineProperty3.default)(_ref, 'status', String), (0, _defineProperty3.default)(_ref, 'brand', { type: ObjectId, ref: 'Brand' }), (0, _defineProperty3.default)(_ref, 'description', String), (0, _defineProperty3.default)(_ref, 'variants', [{ image: String, sku: String, name: String, price: Number, size: String, model: String, maxSize: String, formart: String, preview: Object }]), (0, _defineProperty3.default)(_ref, 'features', Array), (0, _defineProperty3.default)(_ref, 'keyFeatures', Array), (0, _defineProperty3.default)(_ref, 'stats', [{ key: String, val: String }]), (0, _defineProperty3.default)(_ref, 'active', { type: Boolean, default: true }), (0, _defineProperty3.default)(_ref, 'uid', String), (0, _defineProperty3.default)(_ref, 'created_at', { type: Date }), (0, _defineProperty3.default)(_ref, 'updated_at', { type: Date, default: Date.now }), (0, _defineProperty3.default)(_ref, 'updated', { type: Date, default: Date.now }), _ref), { versionKey: false });
 
-export default mongoose.model('Product', ProductSchema);
+exports.default = _mongoose2.default.model('Product', ProductSchema);
+//# sourceMappingURL=product.model.js.map
--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -2,14 +2,16 @@
 
 // Development specific configuration
 // ==================================
+
 module.exports = {
 
   // MongoDB connection options
   mongo: {
-    uri: 'mongodb://localhost/mediaboxzim'
+    uri: 'mongodb://127.0.0.1/mediaboxzim'
   },
 
   // Seed database on startup
   seedDB: true
 
 };
+//# sourceMappingURL=development.js.map
